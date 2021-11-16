d91d1ff0ef8ff1d4bbfe50481e2f677f97121fd8
matildaein <82158772+matildaein@users.noreply.github.com>
2021-11-16 18:02:50 +0100

    Lagt till kommentarer

--- Inl3_Att_Designa_Ett_API/apartments/delete.php
+++ Inl3_Att_Designa_Ett_API/apartments/delete.php
@@ -4,6 +4,7 @@ require_once "../functions.php";
 
 $method = $_SERVER["REQUEST_METHOD"];
 
+// Kontrollerar att rätt metod används
 if ($method !== "DELETE") {
     sendJson(
         [
@@ -13,12 +14,15 @@ if ($method !== "DELETE") {
     );
 }
 
+// Hämtar databas och omvandlar till php
 $enteties = loadJson("../database.json");
 $apartments = $enteties["apartments"];
 
+// Hämtar data som skickats med requesten
 $data = file_get_contents("php://input");
 $requestData = json_decode($data, true);
 
+// Kontrollerar att ett 'id' skickats med i requesten
 if (!isset($requestData["id"])) {
     sendJson(
         [
@@ -28,12 +32,19 @@ if (!isset($requestData["id"])) {
     );
 }
 
+// Sparar undan id samt skapar en variabel för att kontrollera om lägenheten hittats
 $id = $requestData["id"];
 $found = false;
 
+// Letar efter lägenheten med samma 'id' som skickats med i requesten
 foreach ($apartments as $index => $apartment) {
     if ($apartment["id"] === $id) {
+        // Uppdaterar $found om lägenheten hittats och 
+        // sparar undan lägenheten i en variabel för senare användning
         $found = true;
+        $deletedApartment = $apartment;
+
+        // Raderar lägenheten ur arrayen och uppdaterar $enteties
         array_splice($apartments, $index, 1);
         $enteties["apartments"] = $apartments;
         
@@ -41,22 +52,22 @@ foreach ($apartments as $index => $apartment) {
     }
 }
 
-
-
+// Om lägenheten inte hittas skickas ett felmeddelande
 if ($found == false) {
     sendJson(
         [
-            "code" => 2,
-            "message" => "Not found"
+            "message" => "Apartment not found"
         ],
         404
     );
 }
 
+// Sparar ner den uppdaterade databasen
 $saved = saveJson("../database.json", $enteties);
 
+// Om "sparandet" gick bra skickas den raderade lägenheten till användaren
 if ($saved == true) {
-    sendJson(["id" => $id]);
+    sendJson($deletedApartment);
 }
 
 ?>
