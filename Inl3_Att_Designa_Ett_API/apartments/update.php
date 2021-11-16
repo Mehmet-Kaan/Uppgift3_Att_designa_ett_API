53266a8e320ed552e43ede603bbb066b49d08232
matildaein <82158772+matildaein@users.noreply.github.com>
2021-11-16 18:03:44 +0100

    Lagt till kommenterar samt uppdaterat kontroll för tomma nycklar

--- Inl3_Att_Designa_Ett_API/apartments/update.php
+++ Inl3_Att_Designa_Ett_API/apartments/update.php
@@ -37,25 +37,44 @@ if (!isset($requestData["owner"]) && !isset($requestData["city"]) && !isset($req
     && !isset($requestData["street_adress"]) && !isset($requestData["apartment_color"])) {   
     sendJson(
         [
-            "code" => 2,
             "message" => "Bad request"
         ],
         400
     );
 }
 
+// Kontrollerar vilka nycklar som finns och att de inte är tomma
+if (isset($requestData["owner"]) && $requestData["owner"] === "" || 
+    isset($requestData["city"]) && $requestData["city"] === "" || 
+    isset($requestData["street_name"]) && $requestData["street_name"] === "" || 
+    isset($requestData["street_adress"]) && $requestData["street_adress"] === "" || 
+    isset($requestData["apartment_color"]) && $requestData["apartment_color"] === "") {
+        sendJson(
+            [
+                "message" => "Bad request"
+            ],
+            400
+        );
+
+}
+
+// Sparar ner id:et och skapar variabler för hela databasen och lägenheter
 $id = $requestData["id"];
 $enteties = loadJson("../database.json");
 $apartments = $enteties["apartments"];
 
+// En variabel som kontrollerar att lägenheten hittats,
+// samt en variabel där den hittade lägenheten sparas
 $found = false;
 $foundApartment = null;
 
 // Går igenom lägenheterna och hittar lägenheten med rätt id
 foreach ($apartments as $index => $apartment) {
     if ($apartment["id"] == $id) {
+        // Lägenheten har hittats
         $found = true;  
         
+        // Kollar vilka nycklar som ska ändras och ändrar dem
         if (isset($requestData["owner"])) {
             $apartment["owner"] = $requestData["owner"];
         }
@@ -76,25 +95,30 @@ foreach ($apartments as $index => $apartment) {
             $apartment["apartment_color"] = $requestData["apartment_color"];
         }
 
-        $apartments[$index] = $apartment;
+        // Ändrar $foundApartment till den uppdaterade lägenheten
         $foundApartment = $apartment;
+        // Ändrar databasen som sedan ska sparas ner med den nya datan
+        $apartments[$index] = $apartment;
         $enteties["apartments"] = $apartments;
+
         // Avbryter loopen
         break;
     }
 }
 
+// Om lägenheten inte hittas skickas ett felmeddelande
 if ($found == false) {
     sendJson(
         [
-            "code" => 2,
             "message" => "The users by `id` does not exist"
         ],
         404
     );
 }
 
+// Sparar den uppdaterade databasen
 saveJson("../database.json", $enteties);
 
+// Skickar tillbaka den uppdaterade lägenheten till användaren
 sendJson($foundApartment);
 ?>
