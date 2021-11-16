a00c3592cc5b37131fe0eb192a3d516fda5fa8c0
MehmetKaan <82370842+Mehmet-Kaan@users.noreply.github.com>
2021-11-16 11:48:57 +0100

    Add files via upload

---
+++ Inl3_Att_Designa_Ett_API/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+function sendJson($data, $statuscode = 200){
+    header("Content-Type: application/json");
+    http_response_code($statuscode);
+    $json = json_encode($data);
+    echo $json;
+    die();
+}
+
+function loadJson($filename) {
+    $json = file_get_contents($filename);
+    return json_decode($json, true); 
+}
+
+function saveJson($filename, $data) {
+    $json = json_encode($data, JSON_PRETTY_PRINT);
+    file_put_contents($filename, $json);
+}
+?>
